b197cf2205c9fe2bc26a2193a0c53cb014b0a2c7
FangQiJun <zfangqj@gmail.com>
2023-08-21 21:49:41 +0800

    aaa

--- main.ino
+++ main.ino
@@ -1,17 +1,7 @@
-#include <List.hpp>
-
-
-String my_string2;
-List<double> my_list;
-
-
 void setup(){
 
 }
 
 
 void loop(){
-  my_string2.concat(String("Block A"));
-  my_list.add(255);
-  my_string2.concat(String("Block A"));
 }
